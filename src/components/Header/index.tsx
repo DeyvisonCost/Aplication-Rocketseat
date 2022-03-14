d58ff03cfcb8dfb133f94baeb53f0b99aaa585cd
Deyvison Costa <deyvisongtr@gmail.com>
2022-03-14 01:13:44 -0300

    posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Header() {
-  // TODOO
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <img src="/Logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
 }
